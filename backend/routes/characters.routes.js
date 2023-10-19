a48cf9f35c6a1d61c2cdad05bd6903cbe5f04c8b
Krystian Jakubczyk <krystianjnl@gmail.com>
2023-10-19 14:39:45 +0200

    lab done

--- backend/routes/characters.routes.js
+++ backend/routes/characters.routes.js
@@ -26,7 +26,44 @@ router.get('/:id', async (req, res) => {
     console.error(error)
   }
 })
+router.post('/', async (req, res) => {
+  try {
+    const responseFromAPI = await fetch('https://ih-crud-api.herokuapp.com/characters')
+    if (responseFromAPI.ok) {
+      const newCharacter = await responseFromAPI.create(req.body)
+      res.json({ character: newCharacter })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+})
 
+router.put('/:id', async (req, res) => {
+  try {
+    const responseFromAPI = await fetch(
+      `https://ih-crud-api.herokuapp.com/characters/${req.params.id}`
+    )
+    if (responseFromAPI.ok) {
+      const characterFromAPI = await responseFromAPI.json()
+      res.json({ character: characterFromAPI })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+})
+router.delete('/:id', async (req, res) => {
+  try {
+    const responseFromAPI = await fetch(
+      `https://ih-crud-api.herokuapp.com/characters/${req.params.id}`
+    )
+    if (responseFromAPI.ok) {
+      await responseFromAPI.findByIdAndDelete(req.params.id)
+      res.json({ message: 'Character deleted' })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+})
 module.exports = router
 
 // https://ih-crud-api.herokuapp.com/characters
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import AllCharactersPage from './pages/AllCharactersPage'
 import CharacterDetails from './pages/CharacterDetails'
 import HomePage from './pages/HomePage'
+import CreateCharacter from "./pages/CreateCharacter"
 import Navbar from './components/Navbar'
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/characters' element={<AllCharactersPage />} />
         <Route path='/characters/:characterId' element={<CharacterDetails />} />
-
+        <Route path='/characters/create' element={<CreateCharacter />} />
         <Route path='*' element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
